da9466c954a8386b15c3dcb4b6d121ba90b70847
Rohail1212 <158626913+Rohail1212@users.noreply.github.com>
2024-02-08 20:11:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const btnE1 = document.
+getElementById("btn")
+
+const birthdayE1 = document.
+getElementById("birthday");
+const resultE1 = document.
+getElementById("result")
+
+
+function calcuateAge(){
+  const birthdayValue = birthdayE1.
+  value;
+  if(birthdayValue === ""){
+    alert("please enter your birthday")
+  }else{
+  const age = getAge(birthdayValue);
+ resultE1.innerText = `Your age is ${age} ${age > 1 ? "year" :
+ " year"} old`;
+}
+}
+
+function getAge(birthdayValue){
+  const currentDate = new Date();
+  const birthdayDate = new Date
+  (birthdayValue)
+  let age = currentDate.getFullYear()-birthdayDate.getFullYear();
+
+  const month = currentDate.getMonth
+  ()-birthdayDate.getMonth();
+
+  if(month<0 || (month === 0 && 
+    currentDate.getDates()< birthdayDate.getDate())){
+      age--
+    }
+
+    return age;
+}
+
+btnE1.addEventListener("click",
+calcuateAge)
